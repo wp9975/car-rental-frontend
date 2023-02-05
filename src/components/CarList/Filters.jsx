59c1cda4a1568ac39963c708937481af33436b7a
Wojciech Pieczonka <51296602+wp9975@users.noreply.github.com>
2023-02-05 14:48:25 +0100

    fix filters car list

--- src/components/CarList/Filters.jsx
+++ src/components/CarList/Filters.jsx
@@ -1,63 +1,54 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 
 import CarsData from "../../assets/data/CarsData";
 
 const Filters = (props) => {
   const [selectedCarType, setSelectedCarType] = useState("Wszystkie");
-  const [selectedCarBrand, setSelectedCarBrand] = useState();
+  const [selectedCarBrand, setSelectedCarBrand] = useState("Wszystkie");
   const [selectedCarPriceCategory, setSelectedCarPriceCategory] =
     useState("Wszystkie");
   const carTypes = Array.from(
     new Set(CarsData.map((item, index) => item.type))
   );
-  const PriceCategories = ["basic", "standard", "medium", "premium"];
+  const PriceCategories = Array.from(
+    new Set(CarsData.map((item, index) => item.category))
+  );
   const carBrands = Array.from(
     new Set(CarsData.map((item, index) => item.brand))
   );
 
   useEffect(() => {
-    filterCarsType(selectedCarType);
-  }, [selectedCarType]);
-  useEffect(() => {
-    filterCarsBrands(selectedCarBrand);
-  }, [selectedCarBrand]);
-  useEffect(() => {
-    filterCarsPriceCategory(selectedCarPriceCategory);
-  }, [selectedCarPriceCategory]);
+    applyFilters();
+  }, [selectedCarBrand, selectedCarPriceCategory, selectedCarType]);
 
-  const filterCarsType = (carType) => {
-    let result;
-    if (carType === "Wszystkie") {
-      result = CarsData;
-    } else {
-      result = CarsData.filter((current) => {
-        return current.type === carType;
+  useEffect(() => {
+    document.getElementById('clearFiltersButton').addEventListener('click', ()=>{
+      setSelectedCarType("Wszystkie");
+      setSelectedCarBrand("Wszystkie");
+      setSelectedCarPriceCategory("Wszystkie");
+    })
+  })
+
+  const applyFilters = () => {
+    let updatedCarsData = CarsData;
+    if (selectedCarType !== "Wszystkie") {
+      updatedCarsData = updatedCarsData.filter((current) => {
+        return current.type === selectedCarType;
       });
     }
-    props.setData(result);
-  };
-  const filterCarsPriceCategory = (carPriceCategory) => {
-    let result;
-    if (carPriceCategory === "Wszystkie") {
-      result = CarsData;
-    } else {
-      result = CarsData.filter((current) => {
-        return current.category === carPriceCategory;
+    if (selectedCarBrand !== "Wszystkie") {
+      updatedCarsData = updatedCarsData.filter((current) => {
+        return current.brand === selectedCarBrand;
       });
     }
-    props.setData(result);
-  };
-  const filterCarsBrands = (carBrand) => {
-    let result;
-    if (carBrand === "Wszystkie") {
-      result = CarsData;
-    } else {
-      result = CarsData.filter((current) => {
-        return current.brand === carBrand;
+    if (selectedCarPriceCategory !== "Wszystkie") {
+      updatedCarsData = updatedCarsData.filter((current) => {
+        return current.category === selectedCarPriceCategory;
       });
     }
-    props.setData(result);
+    props.setData(updatedCarsData);
   };
+
 
   const handleCarTypeChange = (event) => {
     setSelectedCarType(event.target.value);
@@ -69,48 +60,66 @@ const Filters = (props) => {
     setSelectedCarPriceCategory(event.target.value);
   };
 
+
+
+
   return (
-    <div>
-      <div>
-        <div></div>
-      </div>
-      
+    <div className=" py-3 border-b-2 mb-2">
+
       <div className="mx-auto flex flex-wrap items-center justify-center gap-1 max-w-screen-2xl">
-        <div className='p-2 '>
-          <label className="text-lg">Typ nadwozia
-          <select className="block px-3 py-2 bg-white border border-gray-300 rounded-md shadow-sm w-52 focus:outline-none focus:ring-blue focus:border-blue" onChange={handleCarTypeChange}>
-            <option defaultValue={"Wszystkie"}>Wszystkie</option>
-            {carTypes.map((item, index) => (
-              <option key={index} value={item}>
-                {item}
-              </option>
-            ))}
-          </select>
+        <div className="p-2 ">
+          <label className="text-lg">
+            Typ nadwozia
+            <select
+              className="block px-3 py-2 bg-white border border-gray-300 rounded-md shadow-sm w-52 focus:outline-none focus:ring-blue focus:border-blue focus:ring-2"
+              onChange={handleCarTypeChange}
+              value={selectedCarType}
+            >
+              <option defaultValue={"Wszystkie"}>Wszystkie</option>
+              {carTypes.map((item, index) => (
+                <option key={index} value={item}>
+                  {item}
+                </option>
+              ))}
+            </select>
           </label>
         </div>
-        <div className='p-2 '>
-          <label className="text-lg">Marka
-          <select className="block px-3 py-2 bg-white border border-gray-300 rounded-md shadow-sm w-52 focus:outline-none focus:ring-blue focus:border-blue" onChange={handleCarBrandChange}>
-            <option defaultValue={"Wszystkie"}>Wszystkie</option>
-            {carBrands.map((item, index) => (
-              <option key={index} value={item}>
-                {item}
-              </option>
-            ))}
-          </select>
+        <div className="p-2 ">
+          <label className="text-lg">
+            Marka
+            <select
+              className="block px-3 py-2 bg-white border border-gray-300 rounded-md shadow-sm w-52 focus:outline-none focus:ring-blue focus:border-blue focus:ring-2"
+              onChange={handleCarBrandChange}
+              value={selectedCarBrand}
+            >
+              <option defaultValue={"Wszystkie"}>Wszystkie</option>
+              {carBrands.map((item, index) => (
+                <option key={index} value={item}>
+                  {item}
+                </option>
+              ))}
+            </select>
           </label>
         </div>
-        <div className='p-2 '>
-          <label className="text-lg">Kategoria cenowa
-          <select className="block px-3 py-2 bg-white border border-gray-300 rounded-md shadow-sm w-52 focus:outline-none focus:ring-blue focus:border-blue" onChange={handleCarPriceCategoryChange}>
-            <option defaultValue={"Wszystkie"}>Wszystkie</option>
-            {PriceCategories.map((item, index) => (
-              <option key={index} value={item}>
-                {item}
-              </option>
-            ))}
-          </select>
+        <div className="p-2 ">
+          <label className="text-lg">
+            Kategoria cenowa
+            <select
+              className="block px-3 py-2 bg-white border border-gray-300 rounded-md shadow-sm w-52 focus:outline-none focus:ring-blue focus:border-blue focus:ring-2"
+              onChange={handleCarPriceCategoryChange}
+              value={selectedCarPriceCategory}
+            >
+              <option defaultValue={"Wszystkie"}>Wszystkie</option>
+              {PriceCategories.map((item, index) => (
+                <option key={index} value={item}>
+                  {item}
+                </option>
+              ))}
+            </select>
           </label>
+        </div>
+        <div className="p-2 mt-auto">
+          <button id="clearFiltersButton" className="px-6 py-3 bg-blue rounded-lg text-white" >Wyczyść filtry</button>
         </div>
       </div>
     </div>
